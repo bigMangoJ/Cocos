8988fedffd237583e7cb19e3a6ce2a7120cd8d40
bigMangoJ <timojieli@gmail.com>
2018-10-19 16:32:14 +0800

    J:xxx

--- FightFrame/FightFrame/assets/script/Battle/ChooseTarget.ts
+++ FightFrame/FightFrame/assets/script/Battle/ChooseTarget.ts
@@ -28,11 +28,15 @@ class ChooseTarget {
             case eSortType.mLifeDown:
                 return ChooseTarget.sortByLife(list, -1);
             case eSortType.mLifePerUp:
-                return ChooseTarget.sorByLisfPer(list, 1);
+                return ChooseTarget.sortByLisfPer(list, 1);
             case eSortType.mLifePerDown:
-                return ChooseTarget.sorByLisfPer(list, -1);
-            default // 随机排序
-
+                return ChooseTarget.sortByLisfPer(list, -1);
+            case eSortType.mDisUp:
+                return ChooseTarget.sortByDis(list, 1, actor);
+            case eSortType.mDisDown:
+                return ChooseTarget.sortByDis(list, -1, actor);
+            default: // 随机排序
+                return ChooseTarget.sortByRand(list);
         }
     }
 
@@ -49,7 +53,7 @@ class ChooseTarget {
         return list.sort(by)
     }
 
-    private static sorByLisfPer(list: Array<Actor>, dire: number) {
+    private static sortByLisfPer(list: Array<Actor>, dire: number) {
         function by(a: Actor, b: Actor){
             let mAttriA = a.getComponent<AttriComponent>(eComponent.AttriComponent);
             let mAttriB = b.getComponent<AttriComponent>(eComponent.AttriComponent);
@@ -62,11 +66,31 @@ class ChooseTarget {
         return list.sort(by)
     }
 
+    private static sortByDis(list: Array<Actor>, dire: number, actor: Actor) {
+        function by(a: Actor, b: Actor) {
+            let disA = Math.abs(Math.sqrt(Math.pow(a.x-actor.x, 2) + Math.pow(a.y-actor.y, 2)));
+            let disB = Math.abs(Math.sqrt(Math.pow(b.x-actor.x, 2) + Math.pow(b.y-actor.y, 2)));
+
+            return disA > disB ? -dire : dire
+        }
+        return list.sort(by);
+    }
+
     private static sortByRand(list: Array<Actor>) {
         function by() {
             return Math.random() * 100 < 50 ? 1 : -1;
         }
         return list.sort(by);    
+    }
+
+    private static isChoose(list: Array<Actor>) {
+        let listNew = new Array<Actor>();
+        for(let value of list){
+            if(!value.mIsDead && value.mIsChoose){
+                listNew.push(value);
+            }
+        }
+        return listNew;
     }
 
 }
